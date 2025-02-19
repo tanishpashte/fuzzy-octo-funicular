9c04546a34feeee2eb6adf0214a486652d691591
Tanish <tanishpashte.24@gmail.com>
2025-02-20 01:52:06 +0530

    remove home page button on home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,34 @@
   import VerySorryPage from './VerySorryPage';
   import Flower from './Flower';
   import Confirm from './Confirm';
-  import { Link } from 'react-router-dom';
+  import { useLocation, Link } from 'react-router-dom';
 
   function App() {
     return (
-      <Router>
-        <div>
-        {window.location.pathname !== '/' && (
+      <Router> {/* Router wraps the entire app */}
+        <AppContent /> {/* Component using useLocation is a child of Router */}
+      </Router>
+    );
+  }
+  
+  function AppContent() {  // New component to hold the logic
+    const location = useLocation();
+  
+    return (
+      <div>
+        {location.pathname !== '/' && (
           <Link to="/" className="absolute top-4 left-8 bg-[#445069] p-2 rounded-lg font-bold">
             <i className="fa-solid fa-house-chimney mr-2"></i>Back to home
           </Link>
         )}
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/sorry" element={<SorryPage />} />
-            <Route path="/verysorry" element={<VerySorryPage />} />
-            <Route path="/flower" element={<Flower />} />
-            <Route path="/confirm" element={<Confirm />} />
-          </Routes>
-        </div>
-      </Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sorry" element={<SorryPage />} />
+          <Route path="/verysorry" element={<VerySorryPage />} />
+          <Route path="/flower" element={<Flower />} />
+          <Route path="/confirm" element={<Confirm />} />
+        </Routes>
+      </div>
     );
   }
 
